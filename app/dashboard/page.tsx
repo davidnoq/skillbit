46d8fc8c71a065cac008808dcc4652206f4d7f8f
Tyler Haisman <tyler@haisman.net>
2023-10-17 21:52:17 -0400

    Adjustments to dashboard style

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -26,72 +26,84 @@ const Dashboard = () => {
   return (
     <>
       <div className="max-w-screen text-white flex">
-        <div className="bg-gray-800 h-screen flex-1 max-w-xs border-gray-600 border-r p-6 relative">
-          <div className="flex items-center gap-2">
-            <Image
-              src={Logo}
-              alt=""
-              width={110}
-              height={110}
-              style={{ margin: "-30px" }}
-            ></Image>
-            <h1 className="text-white text-3xl">Skillbit</h1>
-          </div>
-          <div className="flex flex-col justify-between mt-6">
-            <ul className="list-none text-gray-500 flex flex-col gap-2">
-              {path == "/dashboard" && (
-                <li className="p-2 rounded-lg flex items-center gap-2 bg-indigo-600 text-white">
+        <div className="bg-slate-800 h-screen border-slate-700 border-r w-72">
+          <div className="fixed bg-slate-800 h-screen border-slate-700 border-r w-72 p-6">
+            <div className="flex items-center gap-2">
+              <Image
+                src={Logo}
+                alt=""
+                width={110}
+                height={110}
+                style={{ margin: "-30px" }}
+              ></Image>
+              <h1 className="text-white text-3xl">Skillbit</h1>
+            </div>
+            <div className="flex flex-col justify-between mt-6">
+              <ul className="list-none text-white flex flex-col gap-2">
+                {path == "/dashboard" && (
+                  <li className="p-2 rounded-lg flex items-center gap-2 bg-indigo-600 bg-opacity-40 text-white shadow-lg border border-indigo-700">
+                    <Image
+                      src={DashboardIconWhite}
+                      alt=""
+                      width={25}
+                      height={25}
+                    ></Image>
+                    <p>Dashboard</p>
+                  </li>
+                )}
+                {path != "/dashboard" && (
+                  <li className="p-2 rounded-lg flex items-center gap-2 hover:bg-white hover:bg-opacity-10 duration-100">
+                    <Image
+                      src={DashboardIcon}
+                      alt=""
+                      width={25}
+                      height={25}
+                    ></Image>
+                    <p>Dashboard</p>
+                  </li>
+                )}
+                <li className="p-2 rounded-lg flex items-center gap-2 hover:bg-white hover:bg-opacity-10 duration-100">
                   <Image
-                    src={DashboardIconWhite}
+                    src={ApplicantsIcon}
                     alt=""
                     width={25}
                     height={25}
                   ></Image>
-                  <p>Dashboard</p>
+                  <p>Applicants</p>
                 </li>
-              )}
-              {path != "/dashboard" && (
-                <li className="p-2 rounded-lg flex items-center gap-2 hover:bg-indigo-100">
+                <li className="p-2 rounded-lg flex items-center gap-2 hover:bg-white hover:bg-opacity-10 duration-100">
                   <Image
-                    src={DashboardIcon}
+                    src={CompanyIcon}
                     alt=""
                     width={25}
                     height={25}
                   ></Image>
-                  <p>Dashboard</p>
+                  <p>Company Profile</p>
                 </li>
-              )}
-              <li className="p-2 rounded-lg flex items-center gap-2 hover:bg-indigo-100">
-                <Image
-                  src={ApplicantsIcon}
-                  alt=""
-                  width={25}
-                  height={25}
-                ></Image>
-                <p>Applicants</p>
-              </li>
-              <li className="p-2 rounded-lg flex items-center gap-2 hover:bg-indigo-100">
-                <Image src={CompanyIcon} alt="" width={25} height={25}></Image>
-                <p>Company Profile</p>
-              </li>
-              <li className="p-2 rounded-lg flex items-center gap-2 hover:bg-indigo-100">
-                <Image src={WorkshopIcon} alt="" width={25} height={25}></Image>
-                <p>Question Workshop</p>
-              </li>
-            </ul>
-            {/* <ul className="list-none text-gray-500 flex flex-col gap-2">
+                <li className="p-2 rounded-lg flex items-center gap-2 hover:bg-white hover:bg-opacity-10 duration-100">
+                  <Image
+                    src={WorkshopIcon}
+                    alt=""
+                    width={25}
+                    height={25}
+                  ></Image>
+                  <p>Question Workshop</p>
+                </li>
+              </ul>
+              {/* <ul className="list-none text-gray-500 flex flex-col gap-2">
               <li className="p-2 rounded-lg flex items-center gap-2 hover:bg-indigo-100">
                 <Image src={SupportIcon} alt="" width={25} height={25}></Image>
                 <p>Contact Support</p>
               </li>
             </ul> */}
+            </div>
           </div>
         </div>
-        <div className="bg-gray-900 h-screen flex-1">
-          <div className="bg-gray-800 border-b border-gray-700 flex justify-between p-3">
-            <div className="flex-1 max-w-xl bg-gray-200 p-2 rounded-lg flex justify-between">
+        <div className="bg-slate-900 flex-1">
+          <div className="bg-slate-800 border-b border-slate-700 flex justify-between p-3">
+            <div className="flex-1 max-w-xl bg-white bg-opacity-5 p-2 rounded-lg flex justify-between border border-slate-700">
               <input
-                className="text-gray-500 bg-transparent focus:outline-none w-full placeholder:text-gray-500"
+                className="text-white bg-transparent focus:outline-none w-full placeholder:text-white"
                 placeholder="Search Anything..."
               ></input>
               <Image src={SearchIcon} alt="" width={25} height={25}></Image>
