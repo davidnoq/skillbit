dcf25a4a1492cbe0faeb3ef1aa38122d972a469f
Tyler Haisman <tyler@haisman.net>
2023-10-25 00:43:15 -0400

    Adding some preliminary styling to code runner page

--- app/tests/[id]/page.tsx
+++ app/tests/[id]/page.tsx
@@ -1,12 +1,26 @@
 "use client";
 
-import Editor from "@monaco-editor/react";
+import Editor, { loader } from "@monaco-editor/react";
 import { useState, useEffect, useRef } from "react";
 import React from "react";
 import { Socket, io } from "socket.io-client";
+import { motion } from "framer-motion";
 import { Terminal } from "xterm";
 import { FitAddon } from "xterm-addon-fit";
 import "xterm/css/xterm.css";
+import Image from "next/image";
+import Logo from "../../../public/assets/branding/logos/logo_mini_transparent_white.png";
+import TerminalIcon from "../../../public/assets/icons/terminal.svg";
+import WindowIcon from "../../../public/assets/icons/window.svg";
+import HTMLIcon from "../../../public/assets/icons/html.svg";
+import CSSIcon from "../../../public/assets/icons/css.svg";
+import JSIcon from "../../../public/assets/icons/javascript.svg";
+import SidebarIcon from "../../../public/assets/icons/sidebar.svg";
+import DropdownIcon from "../../../public/assets/icons/dropdown.svg";
+import SearchIcon from "../../../public/assets/icons/search.svg";
+import ExitIcon from "../../../public/assets/icons/exit.svg";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 const files: {
   [key: string]: { name: string; language: string; value: string };
@@ -56,7 +70,6 @@ const xtermOptions = {
   useStyle: true,
   screenKeys: true,
   cursorBlink: true,
-  cols: 100,
 };
 
 export default function Tests({ params }: { params: { id: string } }) {
@@ -67,13 +80,18 @@ export default function Tests({ params }: { params: { id: string } }) {
   const [socket, setSocket] = useState(null);
   const [iframeKey, setIframeKey] = useState(1);
   const [webServerPort, setWebServerPort] = useState(null);
+  const [showTerminal, setShowTerminal] = useState(true);
+  const [showBrowser, setShowBrowser] = useState(true);
+  const [showSidebar, setShowSidebar] = useState(true);
+
+  const path = usePathname();
 
   const handleEditorChange = (value, event) => {
     socket.emit("codeChange", { fileName, value });
   };
 
   useEffect(async () => {
-    termRef.current = new Terminal();
+    termRef.current = new Terminal(xtermOptions);
     fitAddonRef.current = new FitAddon();
     termRef.current.loadAddon(fitAddonRef.current);
     termRef.current.open(terminalRef.current);
@@ -122,46 +140,242 @@ export default function Tests({ params }: { params: { id: string } }) {
   }, [socket]);
 
   const file = files[fileName];
+
+  //1e293b
+
+  loader.init().then((monaco) => {
+    monaco.editor.defineTheme("myTheme", {
+      base: "vs-dark",
+      inherit: true,
+      rules: [],
+      colors: {
+        "editor.background": "#1e293b",
+      },
+    });
+  });
+
   return (
-    <div className="max-w-screen text-white bg-slate-900 graphPaper min-h-screen flex items-center justify-center overflow-x-hidden">
-      <div className="w-3/4">
-        <div className="flex flex-row justify-between">
-          <button
-            disabled={fileName === "/project/src/App.js"}
-            onClick={() => setFileName("/project/src/App.js")}
-          >
-            script.js
-          </button>
-          <button
-            disabled={fileName === "style.css"}
-            onClick={() => setFileName("style.css")}
-          >
-            style.css
-          </button>
-          <button
-            disabled={fileName === "index.html"}
-            onClick={() => setFileName("index.html")}
-          >
-            index.html
-          </button>
+    <div className="max-w-screen text-white bg-slate-900 graphPaper min-h-screen overflow-x-hidden flex">
+      {showSidebar && (
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -100 }}
+          transition={{
+            duration: 0.2,
+            delay: 0,
+            ease: "backOut",
+          }}
+          className="bg-slate-800 h-screen border-slate-700 border-r w-72 z-20 relative"
+        >
+          <div className="fixed bg-slate-800 border-slate-700 border-r w-72 p-3">
+            <div className="flex flex-col justify-between">
+              <div className="flex-1 max-w-xl bg-white bg-opacity-5 p-2 rounded-lg flex justify-between border border-slate-700 mb-3">
+                <input
+                  className="text-white bg-transparent focus:outline-none w-full placeholder:text-white"
+                  placeholder="Search Anything..."
+                ></input>
+                <Image src={SearchIcon} alt="" width={25} height={25}></Image>
+              </div>
+              <ul className="list-none text-white flex flex-col gap-1">
+                <hr className="border-t-0 border-b border-b-slate-700 mt-1 mb-1" />
+                <div className="flex justify-between items-center">
+                  <p className="text-base">Project Information</p>
+                  <Image
+                    src={DropdownIcon}
+                    alt=""
+                    width={14}
+                    height={14}
+                  ></Image>
+                </div>
+                <hr className="border-t-0 border-b border-b-slate-700 mt-1 mb-1" />
+                <h1 className="text-sm">Prompt:</h1>
+                <p className="text-sm">
+                  You are tasked with building a simple To-Do list application
+                  in React. The application should allow users to add and remove
+                  tasks from their to-do list...
+                </p>
+                <Link href="" className="text-sm">
+                  See more
+                </Link>
+              </ul>
+              <ul className="list-none text-white flex flex-col gap-1 mt-8">
+                <hr className="border-t-0 border-b border-b-slate-700 mt-1 mb-1" />
+                <div className="flex justify-between items-center">
+                  <p className="text-base">Project Files</p>
+                  <Image
+                    src={DropdownIcon}
+                    alt=""
+                    width={14}
+                    height={14}
+                  ></Image>
+                </div>
+                <hr className="border-t-0 border-b border-b-slate-700 mt-1 mb-1" />
+                <li
+                  disabled={fileName === "/project/src/App.js"}
+                  onClick={() => setFileName("/project/src/App.js")}
+                  className={
+                    fileName === "/project/src/App.js"
+                      ? "p-1 rounded-lg flex items-center gap-2 bg-indigo-600 duration-100"
+                      : "p-1 rounded-lg flex items-center gap-2 hover:bg-slate-700 duration-100"
+                  }
+                >
+                  <Image
+                    src={JSIcon}
+                    alt=""
+                    width={15}
+                    height={15}
+                    className="ml-1 rounded-sm"
+                  ></Image>
+                  <p>script.js</p>
+                </li>
+                <li
+                  disabled={fileName === "style.css"}
+                  onClick={() => setFileName("style.css")}
+                  className={
+                    fileName === "style.css"
+                      ? "p-1 rounded-lg flex items-center gap-2 bg-indigo-600 duration-100"
+                      : "p-1 rounded-lg flex items-center gap-2 hover:bg-slate-700 duration-100"
+                  }
+                >
+                  <Image
+                    src={CSSIcon}
+                    alt=""
+                    width={15}
+                    height={15}
+                    className="ml-1"
+                  ></Image>
+                  <p>style.css</p>
+                </li>
+                <li
+                  disabled={fileName === "index.html"}
+                  onClick={() => setFileName("index.html")}
+                  className={
+                    fileName === "index.html"
+                      ? "p-1 rounded-lg flex items-center gap-2 bg-indigo-600 duration-100"
+                      : "p-1 rounded-lg flex items-center gap-2 hover:bg-slate-700 duration-100"
+                  }
+                >
+                  <Image
+                    src={HTMLIcon}
+                    alt=""
+                    width={15}
+                    height={15}
+                    className="ml-1"
+                  ></Image>
+                  <p>index.html</p>
+                </li>
+                <hr className="border-t-0 border-b border-b-slate-700 mt-1 mb-1" />
+              </ul>
+            </div>
+          </div>
+        </motion.div>
+      )}
+      <div className="flex-1 flex flex-col h-screen">
+        <div className="bg-slate-800 border-b border-slate-700 flex justify-between p-3">
+          <div className="flex-1 flex gap-2">
+            <div className="flex justify-start">
+              <Image
+                src={SidebarIcon}
+                alt=""
+                width={20}
+                height={20}
+                onClick={() => {
+                  if (showSidebar) {
+                    setShowSidebar(false);
+                  } else {
+                    setShowSidebar(true);
+                  }
+                }}
+              ></Image>
+            </div>
+          </div>
+          <div className="flex items-center gap-2">
+            <Image
+              src={Logo}
+              alt=""
+              width={80}
+              height={80}
+              style={{ margin: "-20px" }}
+            ></Image>
+            <h1 className="text-white text-2xl">Skillbit</h1>
+          </div>
+          <div className="flex-1 flex justify-end gap-2">
+            <div className="flex">
+              <Image
+                src={TerminalIcon}
+                alt=""
+                width={20}
+                height={20}
+                onClick={() => {
+                  if (showTerminal) {
+                    setShowTerminal(false);
+                  } else {
+                    setShowTerminal(true);
+                  }
+                }}
+              ></Image>
+            </div>
+            <div className="flex">
+              <Image
+                src={WindowIcon}
+                alt=""
+                width={20}
+                height={20}
+                onClick={() => {
+                  if (showBrowser) {
+                    setShowBrowser(false);
+                  } else {
+                    setShowBrowser(true);
+                  }
+                }}
+              ></Image>
+            </div>
+          </div>
         </div>
-        <Editor
-          height="80vh"
-          theme="vs-dark"
-          path={file.name}
-          defaultLanguage={file.language}
-          defaultValue={file.value}
-          onChange={handleEditorChange}
-        />
-      </div>
-      <div>
-        <div ref={terminalRef} style={{ height: "90%" }}></div>
-        <div className="flex flex-col">
-          <button onClick={() => setIframeKey(iframeKey + 1)}>Reload</button>
-          <iframe
-            key={iframeKey}
-            src={`http://localhost:${webServerPort}`}
-          ></iframe>
+        <div className="h-full flex relative">
+          <div className="flex-1 relative">
+            <Editor
+              theme="myTheme"
+              path={file.name}
+              defaultLanguage={file.language}
+              defaultValue={file.value}
+              onChange={handleEditorChange}
+              className="absolute left-0 right-0 bottom-0 top-0"
+            />
+          </div>
+          {showBrowser && (
+            <motion.div
+              initial={{ opacity: 0, x: 100 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: 100 }}
+              transition={{
+                duration: 0.2,
+                delay: 0,
+                ease: "backOut",
+              }}
+              className="flex-1 relative bg-slate-900"
+            >
+              <iframe
+                className="w-full h-full"
+                key={iframeKey}
+                src={`http://localhost:${webServerPort}`}
+              ></iframe>
+            </motion.div>
+          )}
+          <div
+            className="absolute left-0 right-0 bottom-0 z-30 p-6 bg-black"
+            style={{ display: showTerminal ? "block" : "none" }}
+          >
+            <div ref={terminalRef}></div>
+            <Image
+              src={ExitIcon}
+              alt=""
+              width={15}
+              height={15}
+              className="absolute top-3 right-3"
+            ></Image>
+          </div>
         </div>
       </div>
     </div>
