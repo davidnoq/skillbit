986a4de8a3448234bf9877a9bf903a207a93e7f5
Tyler Haisman <tyler@haisman.net>
2023-11-19 16:04:11 -0500

    Changes to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -26,54 +26,52 @@ export default function Home() {
 
   return (
     <>
-      <Mousetrail></Mousetrail>
-      <section id="home"></section>
-      <Nav></Nav>
-      <div className="max-w-screen text-white graphPaper  py-6 text-center bg-slate-900">
-        <div className=" max-w-screen-xl m-auto">
-          <div className="flex  justify-between items-center flex-row gap-28 mt-28 relative z-10">
-            <div className="basis-1/2">
-              <div className="meshGradient3"></div>
-              <p className="px-2 bg-indigo-600 bg-opacity-10 border border-indigo-600 border-opacity-50 w-fit rounded-lg">
-                Introducing Skillbit
-              </p>
-              <h1 className="text-left text-5xl mt-4 ">
-                Technical interviews, reimagined.
-              </h1>
-              <p className="mt-4 text-left">
-                Take your company's technical recruitment to the next level with
-                Skillbit's AI-powered technical interview platform.
-              </p>
-              <div className="flex items-center gap-28 mt-4">
-                {" "}
-                <button
-                  onClick={() => router.push("/dashboard")}
-                  className="bg-indigo-600 bg-opacity-50 border border-white border-opacity-50 px-6 py-3 rounded-lg flex justify-center items-center hover:bg-opacity-100 hover:bg-indigo-950 duration-200 hover:shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#4f46e5,0_0_15px_#4f46e5,0_0_30px_#4f46e5]"
-                >
-                  Get Started{" "}
-                  <div className=" arrow flex items-center justify-center">
-                    <div className="arrowMiddle"></div>
-                    <div>
-                      <Image
-                        src={Arrow}
-                        alt=""
-                        width={14}
-                        height={14}
-                        className="arrowSide"
-                      ></Image>
+      {/* <Mousetrail></Mousetrail> */}
+      {/* PAGE */}
+      <div className="max-w-screen text-white graphPaper text-center bg-slate-900">
+        <div className="max-w-7xl m-auto px-6">
+          {/* PAGE COVER */}
+          <div className="">
+            <Nav></Nav>
+            <div className="flex justify-between items-center">
+              <div className="">
+                <p className="px-2 bg-white bg-opacity-25 w-fit rounded-lg">
+                  Introducing Skillbit
+                </p>
+                <h1 className="text-left text-5xl mt-4 ">
+                  Technical interviews, reimagined.
+                </h1>
+                <p className="mt-4 text-left">
+                  Take your company's technical recruitment to the next level
+                  with Skillbit's AI-powered technical interview platform.
+                </p>
+                <div className="flex items-center gap-28 mt-4">
+                  {" "}
+                  <button
+                    onClick={() => router.push("/dashboard")}
+                    className="bg-indigo-600 px-6 py-3 rounded-lg flex justify-center items-center"
+                  >
+                    Get Started{" "}
+                    <div className=" arrow flex items-center justify-center">
+                      <div className="arrowMiddle"></div>
+                      <div>
+                        <Image
+                          src={Arrow}
+                          alt=""
+                          width={14}
+                          height={14}
+                          className="arrowSide"
+                        ></Image>
+                      </div>
                     </div>
-                  </div>
-                </button>
+                  </button>
+                </div>
               </div>
-            </div>
-            <div className="w-fit demoBg max-w-5xl mb-28">
-              <Image src={Demo} alt=""></Image>
+              <div className=""></div>
             </div>
           </div>
+          <About></About>
         </div>
-        <About></About>
-        <section id="about"></section>
-        <div className="from-indigo-900 to-transparent "></div>
       </div>
     </>
   );
--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -17,22 +17,8 @@ const Nav = () => {
 
   return (
     <div>
-      <div className="fixed w-full top-0   backdrop-blur-md justify-between items-center gap-20 z-20 flex flex-row">
-        <ul className=" ml-[10px] left-10 basis-1/3 list-none flex gap-6">
-          <li className="opacity-60 hover:cursor-pointer hover:opacity-100 transition duration-500 hover:scale-125">
-            <div onClick={() => scrolltoHash("home")}>Home</div>
-          </li>
-          <li className="opacity-60 hover:cursor-pointer hover:opacity-100 transition duration-500 hover:scale-125">
-            <div onClick={() => scrolltoHash("about")}>About</div>
-          </li>
-          <li className="opacity-60 hover:cursor-pointer hover:opacity-100 transition duration-500 hover:scale-125">
-            Spacer
-          </li>
-          <li className="opacity-60 hover:cursor-pointer hover:opacity-100 transition duration-500 hover:scale-125">
-            Contact
-          </li>
-        </ul>
-        <div className="ml-[-10px] basis-1/3 flex justify-center items-center gap-2">
+      <div className="backdrop-blur-md justify-between items-center gap-20 z-20 flex flex-row py-6">
+        <div className="flex justify-center items-center gap-2">
           <Image
             src={Logo}
             alt=""
@@ -42,7 +28,17 @@ const Nav = () => {
           ></Image>
           <h1 className="">Skillbit</h1>
         </div>
-        <div className="mr-[10px] basis-1/3 flex justify-end ">
+        <ul className="list-none flex gap-6 items-center justify-center">
+          <li className="hover:cursor-pointer transition">
+            <div onClick={() => scrolltoHash("home")}>Home</div>
+          </li>
+          <li className="hover:cursor-pointer transition">
+            <div onClick={() => scrolltoHash("about")}>About</div>
+          </li>
+          <li className="hover:cursor-pointer transition">Spacer</li>
+          <li className="hover:cursor-pointer transition">Contact</li>
+        </ul>
+        <div className="flex justify-end ">
           <button
             onClick={() => router.push("/dashboard")}
             className="bg-indigo-600 bg-opacity-50 border border-white border-opacity-50 px-6 py-3 rounded-lg flex justify-center items-center hover:bg-opacity-100 hover:bg-indigo-950 duration-200 hover:shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#4f46e5,0_0_15px_#4f46e5,0_0_30px_#4f46e5]"
