5175b1c0718c652034154aabb3f62fa373da992b
Blake Rand <blake.rand@icloud.com>
2023-11-28 13:10:43 -0500

    Added api/aiTest route to test with gpt

---
+++ app/api/aiTest/route.ts
@@ -0,0 +1,30 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI();
+
+export async function POST(req: Request) {
+  const body = await req.json();
+  const prompt = body.prompt;
+
+  if (!prompt) {
+    return Response.json({
+      prompt,
+    });
+  }
+
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: "You are a helpful assistant designed to output JSON.",
+      },
+      { role: "user", content: prompt },
+    ],
+    model: "gpt-3.5-turbo-1106",
+
+    response_format: { type: "json_object" },
+  });
+  return Response.json({
+    response: completion.choices[0].message.content,
+  });
+}
