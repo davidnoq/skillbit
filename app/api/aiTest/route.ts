5eeb15d31069b0f29ee95672819744e41a3a80ba
Tyler Haisman <tyler@haisman.net>
2024-04-17 17:49:10 -0400

    Commenting out aitest code for deployment

--- app/api/aiTest/route.ts
+++ app/api/aiTest/route.ts
@@ -1,30 +1,29 @@
-import OpenAI from "openai";
+//commented out for deployment
 
-const openai = new OpenAI();
+// import OpenAI from "openai";
+
+// const openai = new OpenAI();
 
 export async function POST(req: Request) {
-  const body = await req.json();
-  const prompt = body.prompt;
-
-  if (!prompt) {
-    return Response.json({
-      prompt,
-    });
-  }
-
-  const completion = await openai.chat.completions.create({
-    messages: [
-      {
-        role: "system",
-        content: "You are a helpful assistant designed to output JSON.",
-      },
-      { role: "user", content: prompt },
-    ],
-    model: "gpt-3.5-turbo-1106",
-
-    response_format: { type: "json_object" },
-  });
-  return Response.json({
-    response: completion.choices[0].message.content,
-  });
+  // const body = await req.json();
+  // const prompt = body.prompt;
+  // if (!prompt) {
+  //   return Response.json({
+  //     prompt,
+  //   });
+  // }
+  // const completion = await openai.chat.completions.create({
+  //   messages: [
+  //     {
+  //       role: "system",
+  //       content: "You are a helpful assistant designed to output JSON.",
+  //     },
+  //     { role: "user", content: prompt },
+  //   ],
+  //   model: "gpt-3.5-turbo-1106",
+  //   response_format: { type: "json_object" },
+  // });
+  // return Response.json({
+  //   response: completion.choices[0].message.content,
+  // });
 }
