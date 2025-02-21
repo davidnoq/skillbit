977727b15c47a0f51ca3ccfb4590d8e7c21616cb
Tyler Haisman <tyler@haisman.net>
2025-02-21 15:58:06 -0500

    Configuring the view submission screen for recruiters with the new web containers implementation

--- next.config.js
+++ next.config.js
@@ -1,32 +1,69 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = {
+  ...nextConfig,
   webpack: (
     config,
     { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
   ) => {
-    // Important: return the modified config
-
-    // if (isServer) {
-    //   config.externals = config.externals || [];
-
-    //   // Ignore all node_modules from the server build
-    //   config.externals.push(
-    //     new webpack.IgnorePlugin({ resourceRegExp: /node_modules/ })
-    //   );
-
-    //   // Set the target to Node.js since we're running in a Node environment on the server
-    //   config.target = "node";
-    // }
-
     // Add a rule for .node files
     config.module.rules.push({
       test: /\.node$/,
       loader: "node-loader",
     });
 
-    // Return the modified config
     return config;
   },
 };
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// module.exports = {
+//   webpack: (
+//     config,
+//     { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+//   ) => {
+//     // Important: return the modified config
+
+//     // if (isServer) {
+//     //   config.externals = config.externals || [];
+
+//     //   // Ignore all node_modules from the server build
+//     //   config.externals.push(
+//     //     new webpack.IgnorePlugin({ resourceRegExp: /node_modules/ })
+//     //   );
+
+//     //   // Set the target to Node.js since we're running in a Node environment on the server
+//     //   config.target = "node";
+//     // }
+
+//     // Add a rule for .node files
+//     config.module.rules.push({
+//       test: /\.node$/,
+//       loader: "node-loader",
+//     });
+
+//     // Return the modified config
+//     return config;
+//   },
+// };
